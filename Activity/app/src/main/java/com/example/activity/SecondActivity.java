02e0d5006991f050d72dd5b73be24fd431f422f7
317363184 <317363184@qq.com>
2018-03-13 00:49:52 +0800

    add SecondActivity and DialogActivity

--- Activity/app/src/main/java/com/example/activity/SecondActivity.java
+++ Activity/app/src/main/java/com/example/activity/SecondActivity.java
@@ -1,8 +1,19 @@
 package com.example.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -11,7 +22,43 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.e("test", "Create SecondActivity");
         setContentView(R.layout.activity_second);
+        Button toast = (Button) findViewById(R.id.Toast);
+        toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //不会pause
+                //dialog();
+
+                Intent intent = new Intent(SecondActivity.this, DialogActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
+
+    //不会pause
+//    private void dialog(){
+//        final ProgressDialog dialog = new ProgressDialog(this);
+//        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+//        dialog.setMessage("正在加载中");
+//        dialog.setMax(100);
+//        final Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            int progress = 0;
+//
+//            @Override
+//            public void run() {
+//                dialog.setProgress(progress += 5);
+//                if (progress == 100) {
+//                    timer.cancel();
+//                }
+//            }
+//        }, 0, 1000);
+//        dialog.show();
+//    }
 
     @Override
     protected void onStart() {
